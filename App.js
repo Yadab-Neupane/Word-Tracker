ecfc0c8a68a1496e6536920661510de642d05006
Susom <susomshrestha@gmail.com>
2023-05-27 10:09:14 -0400

    Add Test screen in bottom navigation

--- App.js
+++ App.js
@@ -11,113 +11,112 @@ import { MaterialIcons } from '@expo/vector-icons';
 import Header from './src/components/Header';
 import { useState } from 'react';
 import WordList from './src/components/WordList';
-import FormScreen from './src/screens/FormScreen'
+import TestScreen from './src/screens/TestScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+	const [words, setWords] = useState([
+		{
+			title: 'Car',
+			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
+		},
+		{
+			title: 'Dog',
+			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
+		},
+		{
+			title: 'Cat',
+			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
+		},
+		// {
+		//   word: "Cow",
+		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
+		// },
+		// {
+		//   word: "Hair",
+		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
+		// },
+		// {
+		//   word: "Phone",
+		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
+		// },
+		// {
+		//   word: "Mobile",
+		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
+		// },
+	]);
 
-  const [words, setWords] = useState([
-    {
-      title: "Car",
-      description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    },
-    {
-      title: "Dog",
-      description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    },
-    {
-      title: "Cat",
-      description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    },
-    // {
-    //   word: "Cow",
-    //   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    // },
-    // {
-    //   word: "Hair",
-    //   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    // },
-    // {
-    //   word: "Phone",
-    //   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    // },
-    // {
-    //   word: "Mobile",
-    //   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-    // },
+	const onAddNewWord = (title, description) => {
+		const newWord = {
+			title: title,
+			description: description,
+		};
+		words.push(newWord);
+		console.log('New Word ', title);
+	};
 
+	return (
+		<NavigationContainer>
+			<View style={mainStyle.container}>
+				<StatusBar style="auto" />
+				<Header />
 
-  ])
+				<Tab.Navigator>
+					<Tab.Screen
+						name="Home"
+						options={{
+							tabBarInactiveTintColor: lavenderColor,
+							tabBarActiveTintColor: secondaryColor,
+							headerShown: false,
+							tabBarIcon: ({ focused, color, size }) => {
+								const icon = focused ? 'ios-home-sharp' : 'home-outline';
+								return <Ionicons name={icon} size={24} color={color} />;
+							},
+						}}>
+						{(props) => {
+							return <Homescreen {...props} words={words} />;
+						}}
+					</Tab.Screen>
 
-  const onAddNewWord = (title, description) => {
-    const newWord = {
-      title: title,
-      description: description
-    }
-    words.push(newWord)
-    console.log("New Word ", title)
-  }
-
-  return (
-    <NavigationContainer>
-      <View style={mainStyle.container}>
-        <StatusBar style="auto" />
-        <Header />
-
-        <Tab.Navigator>
-          <Tab.Screen
-            name='Home'
-
-            options={{
-              tabBarInactiveTintColor: lavenderColor,
-              tabBarActiveTintColor: secondaryColor,
-              headerShown: false,
-              tabBarIcon: ({ focused, color, size }) => {
-                const icon = focused ? "ios-home-sharp" : "home-outline"
-                return <Ionicons name={icon} size={24} color={color} />
-              }
-            }}
-          >
-            {(props) => {
-              return (
-                <Homescreen
-                  {...props}
-                  words={words}
-                />
-              )
-            }}
-
-          </Tab.Screen>
-
-          <Tab.Screen
-            name='Word'
-
-            options={{
-              tabBarInactiveTintColor: lavenderColor,
-              tabBarActiveTintColor: secondaryColor,
-              headerShown: false,
-              tabBarIcon: ({ focused, color, size }) => {
-                const icon = focused ? "category" : "category"
-                return <MaterialIcons name={icon} size={size} color={color} />
-              }
-            }}
-          // component={WordListScreen}
-          >
-            {(props) => {
-              return (
-                <WordListScreen
-                  {...props}
-                  words={words}
-                  onAddNewWord={onAddNewWord}
-                />
-              )
-            }}
-          </Tab.Screen>
-        </Tab.Navigator>
-
-      </View >
-    </NavigationContainer >
-
-  );
+					<Tab.Screen
+						name="Word"
+						options={{
+							tabBarInactiveTintColor: lavenderColor,
+							tabBarActiveTintColor: secondaryColor,
+							headerShown: false,
+							tabBarIcon: ({ focused, color, size }) => {
+								const icon = focused ? 'category' : 'category';
+								return <MaterialIcons name={icon} size={size} color={color} />;
+							},
+						}}
+						// component={WordListScreen}
+					>
+						{(props) => {
+							return (
+								<WordListScreen
+									{...props}
+									words={words}
+									onAddNewWord={onAddNewWord}
+								/>
+							);
+						}}
+					</Tab.Screen>
+					<Tab.Screen
+						name="Test"
+						component={TestScreen}
+						options={{
+							tabBarInactiveTintColor: lavenderColor,
+							tabBarActiveTintColor: secondaryColor,
+							headerShown: false,
+							tabBarIcon: ({ focused, color, size }) => {
+								const icon = focused ? 'category' : 'category';
+								return <MaterialIcons name={icon} size={size} color={color} />;
+							},
+						}}
+					/>
+				</Tab.Navigator>
+			</View>
+		</NavigationContainer>
+	);
 }
