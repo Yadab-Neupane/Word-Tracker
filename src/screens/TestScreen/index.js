20ffe9359d56e1f1bf7e07b3089c5bd7ed1e3674
Susom <susomshrestha@gmail.com>
2023-05-28 11:35:38 -0400

    add memory box in test screen

--- src/screens/TestScreen/index.js
+++ src/screens/TestScreen/index.js
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import styles from './styles';
+import { Feather } from '@expo/vector-icons';
 
 export default function TestScreen() {
-    return (
-        <View>
-            <Text>Test Screen</Text>
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<View style={styles.box}>
+				<ImageBackground
+					source={require('../../../assets/flashcards.png')}
+					style={styles.image}>
+					<View style={{padding: 30}}>
+						<View style={styles.boxHeader}>
+							<Text style={styles.title}>Memory</Text>
+							<Feather style={styles.info} name="info" size={30} color="black" />
+						</View>
+						<View style={styles.boxBody}>
+							<View>
+								<Text style={styles.content}>Test your memory with</Text>
+								<Text style={styles.content}>our interactive</Text>
+								<Text style={styles.flashcard}>flashcards</Text>
+							</View>
+						</View>
+					</View>
+				</ImageBackground>
+			</View>
+		</View>
+	);
 }
