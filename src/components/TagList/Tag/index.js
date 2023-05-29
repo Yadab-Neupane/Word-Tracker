003586afb4d011041749f0df3880218dfc2ab0ea
Yadab Neupane <yadabneupane@Yadabs-MacBook-Pro.local>
2023-05-29 15:32:38 -0400

    wip: design for adding, editing and removing tags

--- src/components/TagList/Tag/index.js
+++ src/components/TagList/Tag/index.js
@@ -1,14 +1,71 @@
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View, Modal, TextInput } from 'react-native'
 import styles from "./style";
+import { labelWhiteColor } from '../../../common/includes';
+import { useState } from 'react';
 
-export default function Tag({tag}) {
+export default function Tag({ tag }) {
+    const [showEditModal, setShowEditModal] = useState(false);
+
+    const onTagPress = () => {
+        setShowEditModal(!showEditModal);
+    }
+
+    const onTagLongPress = () => {
+        console.log("Tag long pressed");
+    }
+
+    const closeEditModalPress = () => {
+        setShowEditModal(!showEditModal);
+    }
+
+    const onTagChange = () => {
+        console.log("Tag detail changed");
+    }
+
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.tag}>
-                <Text style={styles.text}>
-                    {tag}
-                </Text>
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    style={styles.tag}
+                    onPress={onTagPress}
+                    onLongPress={onTagLongPress}
+                    >
+                    <Text style={styles.text}>
+                        {tag}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <Modal
+                animationType={'slide'}
+                transparent={false}
+                visible={showEditModal}
+                onRequestClose={closeEditModalPress}>
+                <View style={styles.modal}>
+
+                    <TextInput
+                        style={styles.textbox}
+                        onChangeText={onTagChange}
+                        defaultValue={tag}
+                    />
+
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={closeEditModalPress}
+                            underlayColor={labelWhiteColor}>
+                            <Text style={styles.buttonText}>Edit</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={closeEditModalPress}
+                            underlayColor={labelWhiteColor}>
+                            <Text style={styles.buttonText}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </Modal>
+        </>
     )
 }
--- src/components/TagList/Tag/style.js
+++ src/components/TagList/Tag/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native"
-import { blueColor, labelWhiteColor } from "../../../common/includes"
+import { blackShadeColor, blueColor, labelWhiteColor, lavenderColor, primaryColor } from "../../../common/includes"
 
 export default styles = StyleSheet.create({
     container: {
@@ -16,6 +16,45 @@ export default styles = StyleSheet.create({
     text: {
         fontSize: 20,
         color: labelWhiteColor,
-        fontStyle:"italic"
+        fontStyle: "italic"
+    },
+    modal: {
+        flex: 1, 
+        justifyContent: "center", 
+        alignItems: "center",
+        backgroundColor: labelWhiteColor,
+        padding: 10,
+        height: 300
+    },
+    textbox: {
+        borderBottomColor: blackShadeColor,
+        borderBottomWidth: 1,
+        padding: 10,
+        margin: 20,
+        width:'75%',
+        textAlign: 'center',
+        fontSize: 25
+    },
+    buttonContainer:{
+        flexDirection:"row"
+    },
+    button: {
+        marginRight: 20,
+        marginLeft: 20,
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: blueColor,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: labelWhiteColor
+    },
+    buttonText: {
+        color: labelWhiteColor,
+        textAlign: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 20
     }
+
 })
