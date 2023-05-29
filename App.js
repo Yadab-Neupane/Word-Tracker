eb751d568c3038775db8db9802a8bcb78595c556
Susom <susomshrestha@gmail.com>
2023-05-29 11:36:08 -0400

    Add stack navigation for pages without bottom tabs

--- App.js
+++ App.js
@@ -1,121 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Homescreen from './src/screens/Homescreen';
-import WordListScreen from './src/screens/WordListScreen';
 import mainStyle from './src/common/mainStyles';
-import { Ionicons } from '@expo/vector-icons';
-import { lavenderColor, secondaryColor } from './src/common/includes';
-import { MaterialIcons } from '@expo/vector-icons';
 import Header from './src/components/Header';
-import { useState } from 'react';
-import WordList from './src/components/WordList';
-import TestScreen from './src/screens/TestScreen';
+import MemoryComponent from './src/components/Memory';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabScreen from './src/screens/TabScreen';
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-	const [words, setWords] = useState([
-		{
-			title: 'Car',
-			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
-		},
-		{
-			title: 'Dog',
-			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
-		},
-		{
-			title: 'Cat',
-			description: 'four wheel vehicle lorem ipsum hehahahah lorem lorem',
-		},
-		// {
-		//   word: "Cow",
-		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-		// },
-		// {
-		//   word: "Hair",
-		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-		// },
-		// {
-		//   word: "Phone",
-		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-		// },
-		// {
-		//   word: "Mobile",
-		//   description: "four wheel vehicle lorem ipsum hehahahah lorem lorem",
-		// },
-	]);
-
-	const onAddNewWord = (title, description) => {
-		const newWord = {
-			title: title,
-			description: description,
-		};
-		words.push(newWord);
-		console.log('New Word ', title);
-	};
-
 	return (
 		<NavigationContainer>
 			<View style={mainStyle.container}>
 				<StatusBar style="auto" />
 				<Header />
-
-				<Tab.Navigator>
-					<Tab.Screen
-						name="Home"
-						options={{
-							tabBarInactiveTintColor: lavenderColor,
-							tabBarActiveTintColor: secondaryColor,
-							headerShown: false,
-							tabBarIcon: ({ focused, color, size }) => {
-								const icon = focused ? 'ios-home-sharp' : 'home-outline';
-								return <Ionicons name={icon} size={24} color={color} />;
-							},
-						}}>
-						{(props) => {
-							return <Homescreen {...props} words={words} />;
-						}}
-					</Tab.Screen>
-
-					<Tab.Screen
-						name="Word"
-						options={{
-							tabBarInactiveTintColor: lavenderColor,
-							tabBarActiveTintColor: secondaryColor,
-							headerShown: false,
-							tabBarIcon: ({ focused, color, size }) => {
-								const icon = focused ? 'category' : 'category';
-								return <MaterialIcons name={icon} size={size} color={color} />;
-							},
-						}}
-						// component={WordListScreen}
-					>
-						{(props) => {
-							return (
-								<WordListScreen
-									{...props}
-									words={words}
-									onAddNewWord={onAddNewWord}
-								/>
-							);
-						}}
-					</Tab.Screen>
-					<Tab.Screen
-						name="Test"
-						component={TestScreen}
-						options={{
-							tabBarInactiveTintColor: lavenderColor,
-							tabBarActiveTintColor: secondaryColor,
-							headerShown: false,
-							tabBarIcon: ({ focused, color, size }) => {
-								const icon = focused ? 'category' : 'category';
-								return <MaterialIcons name={icon} size={size} color={color} />;
-							},
-						}}
+				<Stack.Navigator>
+					<Stack.Screen
+						name="Tab"
+						component={TabScreen}
+						options={{ headerShown: false }}
 					/>
-				</Tab.Navigator>
+					<Stack.Screen
+						name="Memory"
+						component={MemoryComponent}
+					/>
+				</Stack.Navigator>
 			</View>
 		</NavigationContainer>
 	);
--- src/screens/TestScreen/index.js
+++ src/screens/TestScreen/index.js
@@ -1,29 +1,40 @@
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, TouchableHighlight, View } from 'react-native';
 import styles from './styles';
 import { Feather } from '@expo/vector-icons';
 
-export default function TestScreen() {
+export default function TestScreen(props) {
+
+
+    const onMemoryPressed = () => {
+        props.navigation.push('Memory');
+    }
+
 	return (
 		<View style={styles.container}>
-			<View style={styles.box}>
-				<ImageBackground
-					source={require('../../../assets/flashcards.png')}
-					style={styles.image}>
-					<View style={{padding: 30}}>
-						<View style={styles.boxHeader}>
-							<Text style={styles.title}>Memory</Text>
-							<Feather style={styles.info} name="info" size={30} color="black" />
-						</View>
-						<View style={styles.boxBody}>
-							<View>
-								<Text style={styles.content}>Test your memory with</Text>
-								<Text style={styles.content}>our interactive</Text>
-								<Text style={styles.flashcard}>flashcards</Text>
+			<TouchableHighlight
+				activeOpacity={0.6}
+				underlayColor="#DDDDDD"
+				onPress={onMemoryPressed}>
+				<View style={styles.box}>
+					<ImageBackground
+						source={require('../../../assets/flashcards.png')}
+						style={styles.image}>
+						<View style={{ padding: 30 }}>
+							<View style={styles.boxHeader}>
+								<Text style={styles.title}>Memory</Text>
+								<Feather style={styles.info} name="info" size={30} color="black" />
+							</View>
+							<View style={styles.boxBody}>
+								<View>
+									<Text style={styles.content}>Test your memory with</Text>
+									<Text style={styles.content}>our interactive</Text>
+									<Text style={styles.flashcard}>flashcards</Text>
+								</View>
 							</View>
 						</View>
-					</View>
-				</ImageBackground>
-			</View>
+					</ImageBackground>
+				</View>
+			</TouchableHighlight>
 		</View>
 	);
 }
