f20e9f5d7f7c8dcc116e70b43383464837668d7a
Yadab Neupane <yadabneupane@Yadabs-MacBook-Pro.local>
2023-06-15 19:09:04 -0400

    [#10] Search Funtionality to quickly find specific words Closes #10

--- src/components/WordList/index.js
+++ src/components/WordList/index.js
@@ -1,31 +1,86 @@
-import { ScrollView, TouchableOpacity, View } from 'react-native'
+import { ScrollView, TouchableOpacity, View, TextInput, Button } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from './styles'
 import WordItems from './WordItems'
 import { useEffect, useState } from 'react';
 import * as database from "./../../database/index";
 import { useIsFocused } from "@react-navigation/native";
+import { Feather, Entypo } from "@expo/vector-icons";
 
 
 export default function WordList({ navigation, route, onDeleteWord }) {
     const isFocused = useIsFocused();
+    const [clicked, setClicked] = useState(false);
+    const [searchPhrase, setSearchPhrase] = useState('');
     const [listOfWords, setListOfWords] = useState([]);
+
     useEffect(() => {
-        (async () => {
-            const getAllData = await database.getAllWords();
-            setListOfWords(getAllData);
-        })();
-    }, [isFocused]);
+        if(searchPhrase)
+        {
+            (async () => {
+                const getAllData = await database.getAllWordsByTitle(searchPhrase);
+                setListOfWords(getAllData);
+            })();
+        }
+        else{
+            (async () => {
+                const getAllData = await database.getAllWords();
+                setListOfWords(getAllData);
+            })();
+        }
+       
+    }, [isFocused, searchPhrase]);
+
+    const onSearchCancelled = () => {
+        setClicked(false);
+        setSearchPhrase('');
+    };
+
+    const onSearchFocused = () => {
+        setClicked(true);
+    };
+
+    const onSearchTextChange = (val) => {
+        setSearchPhrase(val);
+    };
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <View style={styles.addButton}>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('Forms')}
+                <View style={styles.searchContainer}>
+                    <View
+                        style={
+                            clicked
+                                ? styles.searchBar__clicked
+                                : styles.searchBar__unclicked
+                        }
                     >
-                        <MaterialIcons name="add-to-photos" size={24} color="black" />
-                    </TouchableOpacity>
+                        <Feather
+                            name="search"
+                            size={20}
+                            color="black"
+                            style={{ marginLeft: 1 }}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Search"
+                            value={searchPhrase}
+                            onChangeText={onSearchTextChange}
+                            onFocus={onSearchFocused}
+                        />
+                        {clicked && (
+                            <Entypo name="cross" size={20} color="black" style={{ padding: 2 }} onPress={onSearchCancelled} />
+                        )}
+                    </View>
+                    <View style={styles.addButton}>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Forms')}
+                        >
+                            <MaterialIcons name="add-to-photos" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
+
 
                 {listOfWords && listOfWords.map((word, index) => {
                     return (<WordItems
--- src/components/WordList/styles.js
+++ src/components/WordList/styles.js
@@ -14,6 +14,7 @@ export default styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
+        marginLeft: 25
     },
     wordListView: {
         borderRadius: 5,
@@ -23,5 +24,35 @@ export default styles = StyleSheet.create({
     },
     content: {
         color: labelWhiteColor
+    },
+    searchContainer: {
+        margin: 2,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "row",
+        width: "90%",
+
+    },
+    searchBar__unclicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "90%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+    },
+    searchBar__clicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "90%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+    input: {
+        fontSize: 18,
+        marginLeft: 10,
+        width: "90%",
     }
 })
