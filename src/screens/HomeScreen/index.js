bc36b9e413053586fcba55b657155e5299fdd975
Bishownath Dhakal <b_dhakal186428@fanshaweonline.ca>
2023-06-20 20:07:30 -0400

    #9 changed the Homescreen to HomeScreen in folder name to avoid build conflict
    Closes #9

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,6 +1,6 @@
 import Home from '../../components/Home'
 
-// Homescreen function name changed to HomeScreen
+// Homescreen function name changed to HomeScreen in screen folder
 export default function HomeScreen({ words }) {
     return (
         <Home
