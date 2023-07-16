09fbd29ff85a06dc34bd20e14736dabea7648055
Yadab Neupane <yadabneupane@Yadabs-MacBook-Pro.local>
2023-07-16 12:50:32 -0400

    removed unused library and removed commented code

--- src/components/ShareWords/index.js
+++ src/components/ShareWords/index.js
@@ -13,15 +13,6 @@ export default function ShareWords({ word }) {
     const shareOnOtherApps = async () => {
         try {
             const result = await Share.share(detailsToShare);
-            // if (result.action === Share.sharedAction) {
-            //     if (result.activityType) {
-            //         // shared with activity type of result.activityType
-            //     } else {
-            //         // shared
-            //     }
-            // } else if (result.action === Share.dismissedAction) {
-            //     // dismissed
-            // }
         } catch (err) {
             console.log(err);
         }
